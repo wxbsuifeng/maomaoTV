118190ce6680fae82f5cbab53c033367ad49c796
wxbsuifeng <1533569724@qq.com>
2021-07-06 22:45:17 +0800

    init:初始化结构

---
+++ web_react/src/views/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.less';
+
+function App() {
+  return (
+    <div className="App">
+      test111
+    </div>
+  );
+}
+
+export default App;
---
+++ web_react/src/views/components/layout/FitLayout.tsx
@@ -0,0 +1,9 @@
+import './index.less'
+
+const FitLayout = (props: any) => {
+  return <div>
+    { props.children }
+  </div>
+}
+
+export default FitLayout
